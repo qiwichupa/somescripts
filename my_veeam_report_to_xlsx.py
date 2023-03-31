1a00a55d76b3951caa0e6e94f4609ee250f80dd3
qiwichupa <qiwichupa@gmail.com>
2023-03-31 14:01:57 +0300

    Update my_veeam_report_to_xlsx.py

--- my_veeam_report_to_xlsx.py
+++ my_veeam_report_to_xlsx.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 #
 # This is htm-to-xlsx converter for " My Veeam Report"  (https://gist.github.com/smasterson/9136468)
-#
+# Usage: ./my_veeam_report_to_xlsx.py veeam-report.htm
 
 import os
 import sys
